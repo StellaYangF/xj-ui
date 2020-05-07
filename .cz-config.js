2620e80a007f6a4389dfd36996ff0761b0caee8b
StellaYangFan <1062878468@qq.com>
2020-05-07 15:06:36 +0800

    chore: 改变 cz 适配器的配置文件

--- .cz-config.js
+++ .cz-config.js
@@ -1,25 +1,28 @@
-'use strict';
-
 module.exports = {
-
   types: [
-    {value: '特性',     name: '特性:    一个新的特性'},
-    {value: '修复',      name: '修复:    修复一个Bug'},
-    {value: '文档',     name: '文档:    变更的只有文档'},
-    {value: '格式',    name: '格式:    空格, 分号等格式修复'},
-    {value: '重构', name: '重构:    代码重构，注意和特性、修复区分开'},
-    {value: '性能',     name: '性能:    提升性能'},
-    {value: '测试',     name: '测试:    添加一个测试'},
-    {value: '工具',    name: '工具:    开发工具变动(构建、脚手架工具等)'},
-    {value: '回滚',   name: '回滚:    代码回退'}
+    { value: "feature", name: "特性: 一个新特性" },
+    { value: "fix", name: "修复: 修复一个 bug" },
+    { value: "docs", name: "文档: 变更的只有文档" },
+    { value: "style", name: "格式: 空格、分号等格式修复" },
+    { value: "refactor", name: "重构: 注意和 bug 修复、新增特性分开" },
+    { value: "perf", name: "性能: 性能提高" },
+    { value: "test", name: "测试: 新增测试" },
+    { value: "chore", name: "杂物: 辅助代码，如生成代码的库" },
+    { value: "revert", name: "撤销: 提交撤销" },
+    { value: "WIP", name: "进程工作: 进程工作" },
   ],
 
   scopes: [
-    {name: '模块1'},
-    {name: '模块2'},
-    {name: '模块3'},
-    {name: '模块4'}
+    { name: "按钮" },
+    { name: "表单" },
+    { name: "功能" },
+    { name: "订单" },
   ],
+
+  allowTicketNumber: false,
+  isTicketNumberRequired: false,
+  ticketNumberPrefix: "TICKET-",
+  ticketNumberRegExp: "\\d{1,5}",
 
   // it needs to match the value for field type. Eg.: 'fix'
   /*
@@ -34,21 +37,27 @@ module.exports = {
   */
   // override the messages, defaults are as follows
   messages: {
-    type: '选择一种你的提交类型:',
-    scope: '选择一个scope (可选):',
+    type: "选择一个你提交的类型：",
+    scope: "选择一个 scope （可选）：",
     // used if allowCustomScopes is true
-    customScope: 'Denote the SCOPE of this change:',
-    subject: '短说明:\n',
-    body: '长说明，使用"|"换行(可选)：\n',
-    breaking: '非兼容性说明 (可选):\n',
-    footer: '关联关闭的issue，例如：#31, #34(可选):\n',
-    confirmCommit: '确定提交说明?'
+    customScope: "指明改变化的 scope：",
+    subject: "祈使句形式简短写出变化：",
+    body:
+      '提供更详细的改变描述，换行（可选）：',
+    breaking: "列出重大改变（可选）：",
+    footer:
+      "List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n",
+    confirmCommit: "Are you sure you want to proceed with the commit above?",
   },
 
   allowCustomScopes: true,
-  allowBreakingChanges: ['特性', '修复'],
+  allowBreakingChanges: ["feat", "fix"],
+  // skip any questions you want
+  skipQuestions: ["footer"],
 
   // limit subject length
-  subjectLimit: 100
-
+  subjectLimit: 100,
+  // breaklineChar: '|', // It is supported for fields body and footer.
+  // footerPrefix : 'ISSUES CLOSED:'
+  // askForBreakingChangeFirst : true, // default is false
 };
