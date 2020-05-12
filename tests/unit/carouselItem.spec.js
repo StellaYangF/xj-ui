ef64d5a87792bfc4a948d0ab6a51ad793558e062
StellaYangFan <1062878468@qq.com>
2020-05-12 09:41:07 +0800

    test(carousel-item): 新增 slot 插槽传入是否符合预期

--- tests/unit/carouselItem.spec.js
+++ tests/unit/carouselItem.spec.js
@@ -3,14 +3,28 @@ import { shallowMount } from '@vue/test-utils';
 import CarouselItem from '@/packages/carousel/CarouselItem.vue';
 
 describe('CarouselItem.vue', () => {
-  it('测试组件是否正常传入 reverse 属性', () => {
+  it('测试是否正常传入 reverse 属性', () => {
     const wrapper = shallowMount(CarouselItem, {
       attachToDocument:true,
-      stubs: {
+      propsData: {
         'reverse': false,
       }
     });
     expect(wrapper.vm.reverse).to.eq(false);
+  });
+
+  it('测试 slot 传入后是否符合预期', () => {
+    const wrapper = shallowMount(CarouselItem, {
+      attachToDocument:true,
+      slots: {
+        default: `
+          <div class='content' style='background:red'>content1</div>
+        `
+      }
+    });
+    const slot = wrapper.findAll('.content');
+    expect(slot.length).to.eq(1);
+    expect(slot.at(0).text()).to.eq('content1');
   });
 
   it ('测试计算属性 isShow 是否正常', () => {
